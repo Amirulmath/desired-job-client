5ae1cd96fbc2930d3aac2aeca5cf7b59f057fdf8
Amirulmath <amirulmath760@gmail.com>
2023-03-23 11:25:10 +0600

    React Hook Form used on Login & SignUp page

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
+import userPhoto from '../../../assets/Profile1.png';
+import logo from '../../../assets/Logo.png'
 
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(err => console.log(err));
+    }
+    
     const menuItems = <>
 
         <li className='font-semibold mr-3'><Link to='/'>Home</Link></li>
-        <li className='font-semibold mr-3'><Link to='/all-parts'>About</Link></li>
-        <li className='font-semibold mr-3'><Link to='/portfolio'>Contact Us</Link></li>
-        <li className='font-semibold mr-3'><Link to='/blogs'>Blog</Link></li>
+        <li className='font-semibold mr-3'><Link to='/about'>About</Link></li>
+        <li className='font-semibold mr-3'><Link to='/contact'>Contact Us</Link></li>
+        <li className='font-semibold mr-3'><Link to='/blog'>Blog</Link></li>
 
     </>
 
@@ -17,7 +28,7 @@ const Navbar = () => {
 
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <label tabIndex="0" className="btn btn-light lg:hidden mr-10">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
@@ -25,9 +36,9 @@ const Navbar = () => {
                             {menuItems}
                         </ul>
                     </div>
-                    <div className="avatar md:block hidden">
+                    <div className="avatar">
                         <div className="w-16 rounded-xl">
-                            {/* <img src={logo} alt="" /> */}
+                            <img src={logo} alt="" />
                         </div>
                     </div>
                 </div>
@@ -49,11 +60,8 @@ const Navbar = () => {
                         <label tabIndex="0">
 
                             <div className="avatar">
-                                <div>Login</div>
-
                                 <div className=" text-center text-white text-xl bg-primary leading-tight z-10 rounded-full md:w-16 w-12  md:h-16 h-12 border-2 border-primary cursor-pointer hover:ring hover:ring-offset-2 duration-500 ring-primary">
-
-                                    {/* <img src={user?.photoURL ? user?.photoURL : userPhoto} alt={user?.displayName} /> */}
+                                    <img src={user?.photoURL ? user?.photoURL : userPhoto} alt={user?.displayName} />
                                 </div>
 
                             </div>
@@ -61,16 +69,15 @@ const Navbar = () => {
                         </label>
 
                         <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-lg md:w-52 h-52">
-                            {/* <p className='text-center mb-5 md:font-bold'>{user?.displayName}</p> */}
+                            <p className='text-center mb-5 md:font-bold'>{user?.displayName}</p>
                             <p className='mb-3 text-center'>
-                                {/* {
-                                    user ? <p className='text-center btn  btn-sm rounded-full' onClick={logout}><Link to='/login'>SignOut</Link></p>
+                                {
+                                    user ? <p className='text-center btn  btn-sm rounded-full' onClick={handleLogOut}><Link to='/login'>SignOut</Link></p>
                                         :
-                                        <p className='text-center btn  btn-sm rounded-full'><Link to='/login'>Login</Link></p>
+                                        <p className='text-center btn  btn-sm rounded-full'><Link to='/login'>Sign In</Link></p>
 
-                                } */}
+                                }
                             </p>
-                            <p className='text-center btn  btn-sm rounded-full'><Link to='/dashboard'>Dashboard</Link></p>
                         </ul>
                     </div>
                 </div>
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,121 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const SignUp = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { createUser, updateUser } = useContext(AuthContext);
+    const [signUpError, setSignUPError] = useState('');
+    const [createdUserEmail, setCreatedUserEmail] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignUp = data => {
+        setSignUPError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                const userInfo = {
+                    displayName: data.name,
+                    specialty: data.specialty
+                }
+                updateUser(userInfo)
+                    .then(() => {
+                        saveUser(data.name, data.email, data.specialty);
+                    })
+                    .catch(err => console.log(err));
+
+            })
+            .catch(error => {
+                console.log(error)
+                setSignUPError(error.message)
+            });
+
+        const saveUser = (name, email, specialty) => {
+            const user = { name, email, specialty };
+            fetch('https://laptop-2hand-server.vercel.app/users', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setCreatedUserEmail(email);
+                })
+        }
+    }
+
+    return (
+        <div>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">If you want to access our services, you need to login first.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-blue-200">
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit(handleSignUp)}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Your Name</span>
+                                    </label>
+                                    <input type="text" {...register("name", {
+                                        required: "Name is Required"
+                                    })} placeholder="name" className="input input-bordered" />
+                                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" {...register("email", {
+                                        required: "Email Address is required"
+                                    })} placeholder="email" className="input input-bordered" />
+                                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password"
+                                        {...register("password", {
+                                            required: "Password is required",
+                                            minLength: { value: 6, message: "Password must be 6 characters long" },
+                                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                                        })}
+                                        placeholder="password" className="input input-bordered" />
+                                    <label className="label"> <span className="label-text">Forget Password?</span></label>
+                                    {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Specialty</span>
+                                    </label>
+                                    <select {...register("specialty", { required: true })} className="select select-bordered w-full max-w-xs">
+                                        <option value="Buyer">Buyer</option>
+                                        <option value="Seller">Seller</option>
+                                    </select>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className='btn btn-primary w-full' value="Sign Up" type="submit" />
+                                </div>
+                                <div>
+                                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                                </div>
+                                <p>Already have an account <Link className='text-secondary' to="/login"><b>Please Login</b></Link></p>
+                                <div className="divider">OR</div>
+                                <p>mn</p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
